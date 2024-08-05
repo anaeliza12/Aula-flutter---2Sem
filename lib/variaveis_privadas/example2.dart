8721f11f74b4f0de8114c96d272de79abc4ff937
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-05 20:28:43 -0300

    main

--- lib/variaveis_privadas/example2.dart
+++ lib/variaveis_privadas/example2.dart
@@ -1,13 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Example {
- final String _name;
+  final String _name;
 
- Example(this._name)
-
+  Example(this._name);
 
   _printName() {
-  debugPrint(_name);
- }
+    debugPrint(_name);
+  }
 
+  printName() {
+    _printName();
+  }
 }
+
+  void main(List<String> args) {
+    final example = Example("Aula 01");
+    example._printName();
+  }
+
